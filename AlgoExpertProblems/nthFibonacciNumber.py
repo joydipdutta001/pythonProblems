50009c99eb73c8b443341b97bfcef7b5330f2f15
joydipdutta001 <53464193+joydipdutta001@users.noreply.github.com>
2020-06-20 15:33:33 +0530

    nthFibonacci.py Completed

--- AlgoExpertProblems/nthFibonacciNumber.py
+++ AlgoExpertProblems/nthFibonacciNumber.py
@@ -5,7 +5,7 @@ def nthFib(n):
     elif n== 1:
         return 0
     else:
-        return nthFib(n-1) +nthFib(n+2)
+        return nthFib(n-1) +nthFib(n-2)
 
 # O(n) time || O(n) space
 def nthFibmore(n, mem= {1:0,2:1}):
@@ -26,3 +26,9 @@ def nthbetterfib(n):
         lastTwo[1]= nexFib
         count +=1
     return lastTwo[1] if n > 1 else lastTwo[0]
+
+n = int(input("Enter the number:> "))
+
+print(str(nthFib(n))+ " "+
+str(nthFibmore(n))+" "+
+str(nthbetterfib(n)))
