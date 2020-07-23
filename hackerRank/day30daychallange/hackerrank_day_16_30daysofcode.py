7ea6ed46cc7dd20d17d017c52212bb84dc18370f
joydipdutta001 <53464193+joydipdutta001@users.noreply.github.com>
2020-07-23 23:23:35 +0530

    hacker rank 30 days challenge updated day16 and day17

---
+++ hackerRank/day30daychallange/hackerrank_day_16_30daysofcode.py
@@ -0,0 +1,26 @@
+""" 
+////////////////////////////////////////////////////////////////////////////////
+                author_name= Joydip Dutta,
+                author_email= duttajoydip98@gmail.com,
+                github_id = joydipdutta001
+                linkedIn_id = Joydip Dutta (JD)
+                Website = cybotians.com
+                
+                Problem No. = Day-16
+                
+                
+ ////////////////// THANK YOU /////////////////////////////////////////////////   
+ """
+
+if __name__ == '__main__':
+    # Write Your code here
+    # !/bin/python3
+
+    import sys
+
+    S = input().strip()
+    try:
+        r = int(S)
+        print(r)
+    except ValueError:
+        print("Bad String")
---
+++ hackerRank/day30daychallange/hackerrank_day_17_30daysofcode.py
@@ -0,0 +1,40 @@
+""" 
+////////////////////////////////////////////////////////////////////////////////
+                author_name= Joydip Dutta,
+                author_email= duttajoydip98@gmail.com,
+                github_id = joydipdutta001
+                linkedIn_id = Joydip Dutta (JD)
+                Website = cybotians.com
+
+                Problem No. = Day-17
+                
+
+                
+                
+ ////////////////// THANK YOU /////////////////////////////////////////////////   
+ """
+
+
+class Calculator(Exception):
+    def power(self, n, p):
+        if (n < 0 or p < 0):
+            raise Calculator("n and p should be non-negative")
+        else:
+            return pow(n, p)
+
+
+if __name__ == '__main__':
+    # Write Your code here
+    # Write your code here
+
+
+
+    myCalculator = Calculator()
+    T = int(input())
+    for i in range(T):
+        n, p = map(int, input().split())
+        try:
+            ans = myCalculator.power(n, p)
+            print(ans)
+        except Exception as e:
+            print(e)
