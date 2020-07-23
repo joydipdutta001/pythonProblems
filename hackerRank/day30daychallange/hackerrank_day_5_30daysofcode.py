556c3d0e11e5b60085a0ba2a76894e06530e560e
joydipdutta001 <53464193+joydipdutta001@users.noreply.github.com>
2020-07-23 10:19:42 +0530

    hacker rank 30 days challenge updated day 04-07

---
+++ hackerRank/day30daychallange/hackerrank_day_5_30daysofcode.py
@@ -0,0 +1,28 @@
+""" 
+////////////////////////////////////////////////////////////////////////////////
+                author_name= Joydip Dutta,
+                author_email= duttajoydip98@gmail.com,
+                github_id = joydipdutta001
+                linkedIn_id = Joydip Dutta (JD)
+                Website = cybotians.com
+                
+                Problem No. = Day-05
+                
+                
+ ////////////////// THANK YOU /////////////////////////////////////////////////   
+ """
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        print("{0} x {1} = {2}".format(n,i,(n*i)))
